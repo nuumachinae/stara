75bc0cbb93fed08bff42e3e41f779326beb68dbe
nuumachinae <nuumachinae@outlook.com>
2023-09-02 06:11:38 -0400

    updated source.

---
+++ source/xos/protocol/http/messages.cpp
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2023 $organization$
+///
+/// This software is provided by the author and contributors ``as is'' 
+/// and any express or implied warranties, including, but not limited to, 
+/// the implied warranties of merchantability and fitness for a particular 
+/// purpose are disclaimed. In no event shall the author or contributors 
+/// be liable for any direct, indirect, incidental, special, exemplary, 
+/// or consequential damages (including, but not limited to, procurement 
+/// of substitute goods or services; loss of use, data, or profits; or 
+/// business interruption) however caused and on any theory of liability, 
+/// whether in contract, strict liability, or tort (including negligence 
+/// or otherwise) arising in any way out of the use of this software, 
+/// even if advised of the possibility of such damage.
+///
+///   File: messages.cpp
+///
+/// Author: $author$
+///   Date: 9/1/2023
+//////////////////////////////////////////////////////////////////////////
+#include "xos/protocol/http/messages.hpp"
+#include "xos/protocol/http/request/message.cpp"
+#include "xos/protocol/http/response/message.cpp"
+#include "xos/protocol/xttp/messages.cpp"
+
+namespace xos {
+namespace protocol {
+namespace http {
+
+} /// namespace http 
+} /// namespace protocol 
+} /// namespace xos 
---
+++ source/xos/protocol/xttp/messages.hpp
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2023 $organization$
+///
+/// This software is provided by the author and contributors ``as is'' 
+/// and any express or implied warranties, including, but not limited to, 
+/// the implied warranties of merchantability and fitness for a particular 
+/// purpose are disclaimed. In no event shall the author or contributors 
+/// be liable for any direct, indirect, incidental, special, exemplary, 
+/// or consequential damages (including, but not limited to, procurement 
+/// of substitute goods or services; loss of use, data, or profits; or 
+/// business interruption) however caused and on any theory of liability, 
+/// whether in contract, strict liability, or tort (including negligence 
+/// or otherwise) arising in any way out of the use of this software, 
+/// even if advised of the possibility of such damage.
+///
+///   File: messages.hpp
+///
+/// Author: $author$
+///   Date: 8/30/2023
+//////////////////////////////////////////////////////////////////////////
+#ifndef XOS_PROTOCOL_XTTP_MESSAGES_HPP
+#define XOS_PROTOCOL_XTTP_MESSAGES_HPP
+
+#include "xos/protocol/xttp/request/message.hpp"
+#include "xos/protocol/xttp/response/message.hpp"
+
+namespace xos {
+namespace protocol {
+namespace xttp {
+
+} /// namespace xttp 
+} /// namespace protocol 
+} /// namespace xos 
+
+#endif /// ndef XOS_PROTOCOL_XTTP_MESSAGES_HPP
